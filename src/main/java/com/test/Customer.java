9894591a9070cf5129287028628ab3626fad4300
Parashar Devashish <devdewance@gmail.com>
2015-07-11 20:38:54 +0530

    Normally, Spring is accepting date variable, here’s a tip to use CustomDateEditor to work around it.

---
+++ src/main/java/com/test/Customer.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import java.util.Date;
+
+public class Customer 
+{
+	
+	
+private Date date ;
+
+public Date getDate() {
+	return date;
+}
+
+public void setDate(Date date) {
+	this.date = date;
+}
+
+@Override
+public String toString() {
+	return "Customer [date=" + date + "]";
+}
+
+	
+}
+	
